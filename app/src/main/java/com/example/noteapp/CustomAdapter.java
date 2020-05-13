1d0972639a5a40ffc80e43e369b093dfb75de686
kazuhisanoguchi <pasuwaado135@gmail.com>
2020-05-14 00:12:52 +0300

    3rd

--- app/src/main/java/com/example/noteapp/CustomAdapter.java
+++ app/src/main/java/com/example/noteapp/CustomAdapter.java
@@ -52,11 +52,12 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
             public void onClick(View view) {
                 Intent intent = new Intent(context, UpdateActivity.class);
                 intent.putExtra("id", holder.note_id_txt.getText().toString());
-                intent.putExtra("title",holder.note_title_txt.getText().toString()  );
+                intent.putExtra("title",holder.note_title_txt.getText().toString());
                 intent.putExtra("author", holder.note_message_txt.getText().toString());
                 activity.startActivityForResult(intent, 1);
             }
         });
+
         /*holder.mainLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,7 +93,31 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
             //Animate Recyclerview
             Animation translate_anim = AnimationUtils.loadAnimation(context, R.anim.translate_anim);
             mainLayout.setAnimation(translate_anim);
+
+            mainLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(context, UpdateActivity.class);
+                    intent.putExtra("id", note_id_txt.getText().toString());
+                    intent.putExtra("title",note_title_txt.getText().toString()  );
+                    intent.putExtra("author",   note_message_txt.getText().toString());
+                    activity.startActivityForResult(intent, 1);
+                }
+            });
         }
+
+        /*MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            note_id_txt = itemView.findViewById(R.id.note_id_txt);
+            note_title_txt = itemView.findViewById(R.id.note_title_txt);
+            note_message_txt = itemView.findViewById(R.id.note_message_txt);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+            //Animate Recyclerview
+            Animation translate_anim = AnimationUtils.loadAnimation(context, R.anim.translate_anim);
+            mainLayout.setAnimation(translate_anim);
+        }
+
+         */
 
     }
 
